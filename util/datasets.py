6222996527a3785dab4ab4afcd853d4d8a7df77c
hqhQAQ <1067436797@qq.com>
2022-12-05 00:26:17 +0800

    Consistency & Stability Score

---
+++ util/datasets.py
@@ -0,0 +1,64 @@
+import os
+from statistics import mean
+from cv2 import transform
+from torchvision.datasets.folder import default_loader
+from torch.utils.data import Dataset
+import pandas as pd
+# from tools.stanford_dogs import Dogs
+
+class Cub2011Eval(Dataset):
+    base_folder = 'images'
+
+    def __init__(self, root, train=True, transform=None, loader=default_loader):
+        self.root = os.path.expanduser(root)
+        self.transform = transform
+        self.loader = default_loader
+        self.train = train
+
+        if not self._check_integrity():
+            raise RuntimeError('Dataset not found or corrupted.' +
+                               ' You can use download=True to download it')
+
+    def _load_metadata(self):
+        images = pd.read_csv(os.path.join(self.root, 'images.txt'), sep=' ',
+                             names=['img_id', 'filepath'])
+        image_class_labels = pd.read_csv(os.path.join(self.root, 'image_class_labels.txt'),
+                                         sep=' ', names=['img_id', 'target'])
+        train_test_split = pd.read_csv(os.path.join(self.root, 'train_test_split.txt'),
+                                       sep=' ', names=['img_id', 'is_training_img'])
+
+        data = images.merge(image_class_labels, on='img_id')
+        self.data = data.merge(train_test_split, on='img_id')
+
+        if self.train:
+            self.data = self.data[self.data.is_training_img == 1]
+        else:
+            self.data = self.data[self.data.is_training_img == 0]
+
+    def _check_integrity(self):
+        try:
+            self._load_metadata()
+        except Exception:
+            return False
+
+        for index, row in self.data.iterrows():
+            filepath = os.path.join(self.root, self.base_folder, row.filepath)
+            if not os.path.isfile(filepath):
+                print(filepath)
+                return False
+        return True
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        sample = self.data.iloc[idx]
+        path = os.path.join(self.root, self.base_folder, sample.filepath)
+        target = sample.target - 1  # Targets start at 1 by default, so shift to 0
+        img = self.loader(path)
+        img_id = sample.img_id
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        return img, target, img_id
